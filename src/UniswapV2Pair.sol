f528a259695c649b2b2b6823922f896be3b9032a
yassine <control.yassine@gmail.com>
2025-04-01 16:30:19 +0000

    small modfications

--- src/UniswapV2Pair.sol
+++ src/UniswapV2Pair.sol
@@ -1,10 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+import {ERC20} from "solmate/tokens/ERC20.sol";
 
-ccontract ZuniswapV2Pair is ERC20, Math {
+
+contract ZuniswapV2Pair is ERC20 {
  
   uint256 private reserve0;
   uint256 private reserve1;
 
 
+    function mint() public {
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+        uint256 amount0 = balance0 - reserve0;
+        uint256 amount1 = balance1 - reserve1;
+
+        uint256 liquidity;
+
+        if (totalSupply == 0) {
+            liquidity = (0);
+            _mint(address(0), MINIMUM_LIQUIDITY);
+        } else {
+            liquidity = (0);
+        }
+
+        if (liquidity <= 0) revert InsufficientLiquidityMinted();
+
+        _mint(msg.sender, liquidity);
+
+        _update(balance0, balance1);
+
+        emit Mint(msg.sender, amount0, amount1);
+    }
 }
+
+
